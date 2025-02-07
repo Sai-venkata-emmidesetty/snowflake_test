cf36a745e1a1aa2f9aae8e10fe45ff9f32605421
sai-venkata-emmidesetty <saivenkatev17@gmail.com>
2025-02-07 09:47:04 +0530

    added

--- main.tf
+++ main.tf
@@ -12,9 +12,27 @@ provider "snowflake" {
   organization_name = "ercpqao"
   user              = "WINTER"
   password          = "Dev@ccountforsnowflake1"
-  role              = "SYSADMIN"
+  role              = "ACCOUNTADMIN"
 }
 
 resource "snowflake_database" "dev_snowflake_database" {
   name = "DEV_DATA_BASE"
+  catalog = ""
+  comment =  ""
+  data_retention_time_in_days = 1
+  default_ddl_collation = ""
+  enable_console_output = false
+  external_volume = ""
+  is_transient = false
+  log_level = "OFF"
+  max_data_extension_time_in_days = 14
+  quoted_identifiers_ignore_case = false
+  replace_invalid_characters = false
+  storage_serialization_policy = "OPTIMIZED"
+  suspend_task_after_num_failures = 10
+  task_auto_retry_attempts = 0
+  trace_level = "OFF"
+  user_task_managed_initial_warehouse_size = "Medium"
+  user_task_minimum_trigger_interval_in_seconds = 30
+  user_task_timeout_ms = 3600000
 }
